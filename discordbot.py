d188f92657cbd7158a74e6367044976420b1166e
Estra47 <130191736+Estra47@users.noreply.github.com>
2023-04-08 08:53:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,26 +4,29 @@ import discord
 from dotenv import load_dotenv
 import os
 load_dotenv()
+from discord import Embed
+from discord.ext import commands
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+intents = discord.Intents.all()
+intents.members = True
 
-@client.event
+app = commands.Bot(command_prefix='PREFIX',intents=intents)
+
+@app.slash_command(name="botlist", description="Bot List") # 슬래시 커맨드 등록
+async def mdlist(ctx: commands.Context): # 슬래시 커맨드 이름
+    embed = Embed(title = "MDstudio Bot List",color=0x00AAFF)
+    embed.set_footer(text="아로나 Arona : MD studio KR support bot")
+    embed.set_footer(text="プラナ Plana : MD studio JP support bot")
+    embed.set_footer(text="GSC : MD studio Global testing bot")
+    await ctx.respond(embed=embed) # 인터렉션 응답
+
+@app.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+    print('Done')
+    await app.change_presence(status=discord.Status.online, activity=None)
 
 
 try:
